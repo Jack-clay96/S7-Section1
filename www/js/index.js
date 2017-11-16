0881def7dd3ad28f91157ce1086790d830fe0fe2
Jack-clay96 <claj2_14@uni.worc.ac.uk>
2017-11-16 15:20:57 +0000

    bool

--- www/js/index.js
+++ www/js/index.js
@@ -1,79 +1,42 @@
-var value = "nexus";
-var key = "phone";
-var value1 = "wallet";
-var key1 = "black";
-//var glasses = "green";
-//var drink = "coke";
-//var food = "burger";
-
-//JSON:
-var choc= {
-    "name" : "twix",
-    "weight" : "100g",
-    "carbs" : "20g"
-};
-
-var chocList = [{
-    "name": "Twirl",
-    "quality": "Good"},
-                
-    {"name": "Flake",
-     "quality": "Average"}];
-        
+$(document).on("pagecreate","#pageone",function(){ //Once page is created run code:
+     $("#submitButton").on("click", buttonClicked); //listenser for when button is clicked.
+    //Once it has run the buttonclicked() fucntion
+});
 
 
 
-window.localStorage.setItem( key, value);
-
-
-document.addEventListener("deviceready", onDeviceReady, false);
-//localStorage2();
+//document.addEventListener("deviceready", onDeviceReady, false);
+var test;
+$(document).ready(onDeviceReady); // used for testing on webpage. document.addeventlisten needs to be commented
 
 
 
 //$ is used in jquery. # = ID. $ is looking at the launched in HTML document. 	
-function updateDisplay() {
+/*function updateDisplay() {
 	$("#launched").text("Application launched: " + launched_count);
-}
+}*/
 
 
 // device APIs are available
 //
     function onDeviceReady() {
 	
-	updateDisplay();  
+	//updateDisplay();  
 	alert("device ready");
-    //localStorage2(); //best to call methods here. As it waits until phone is ready. (Cant test on webpage from here) 
+        
+        
+        
+    //best to call methods here. As it waits until phone is ready. (Cant test on webpage from here)
     }
 
-
-
-    function localStorage2() //Function name can't be something used for something else
-    {
-        window.localStorage.setItem( key, value );
-        var getPhone = window.localStorage.getItem(key);
-        alert("Phone: " + getPhone);
-        
-         window.localStorage.setItem( key1, value1 );
-        var getPhone = window.localStorage.getItem(key1);
-        $("#key1").text("Wallet: " + key1); //#key1 is being replaced by wallet - Used for positioning on page within html document
-		
-        var items = window.localStorage.length;
-        console.log("No. item stored: " + items);
-        
-    }
-
-function jsonString()
-{
-    var myJSON = JSON.stringify(choc); //turn the var choc into a string
-    console.log("my favorite chocolate: " + choc.name + " and  weight is: " + choc.weight);
+function random() {
+    
+    return !Math.round(Math.random()); //Returns true or flase (its random)
 }
 
-function jsonList()
+function buttonClicked()
 {
-    var output = '';
-    $.each(chocList, function(index, value){
-           output += '<li>' +value.name + '</li>';
-           });
-    $('#searchChoc').html(output).listview("refresh");
+    
+    console.log("buttonClicked");
+    $("#myText").text(random); //myText matches with span
 }
